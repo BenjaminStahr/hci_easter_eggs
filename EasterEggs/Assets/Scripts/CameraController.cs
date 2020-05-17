2e5501d1d8da9642fabf9895c501627a5b08a34c
BenjaminStahr <BenjaminStahr97@web.de>
2020-05-17 14:39:56 +0200

    first prototype, is able to locate egg by coloring knob

---
+++ EasterEggs/Assets/Scripts/CameraController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CameraController : MonoBehaviour
+{
+    GameObject SearchKnob;
+    //public Material KnobMaterial;
+    // Start is called before the first frame update
+    void Start()
+    {
+        SearchKnob = GameObject.FindGameObjectWithTag("SearchKnob");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GameObject EasterEgg = GameObject.FindGameObjectWithTag("EasterEgg");
+        Vector3 EasterEggVector = EasterEgg.transform.position - this.transform.position;
+        float AngleEasterEgg = Vector3.Angle(EasterEggVector, transform.TransformDirection(Vector3.forward));
+        if (AngleEasterEgg < 60)
+        {
+            Debug.Log("Value : "+(255 - (60 - (AngleEasterEgg / 3)) * 4.25));
+            Debug.Log("Angle : "+AngleEasterEgg);
+            SearchKnob.GetComponent<GvrReticlePointer>().MaterialComp.color = new Color32(255, (byte)(255 - (60 - AngleEasterEgg) * 4.25), (byte)(255 - (60 - AngleEasterEgg) * 4.25), 255);
+        }
+        else
+        {
+            SearchKnob.GetComponent<GvrReticlePointer>().MaterialComp.color = new Color32(255, 255, 255, 255);
+        }
+        
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, 100, ~0))
+        {
+            if (hit.transform.gameObject.tag == "EasterEgg")
+            {
+                hit.transform.GetComponent<MeshRenderer>().enabled = true;
+            }
+        }
+    }
+}
