c0ff0cbf401f0bc0bea9df190de89984de942231
BenjaminStahr <BenjaminStahr97@web.de>
2020-06-03 18:22:48 +0200

    added first menu version

---
+++ EasterEggs/Assets/Scripts/LoadGameScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class LoadGameScript : MonoBehaviour
+{
+    public bool Holding = false;
+    public bool TimeSetted = false;
+    public double time = 0;
+    // Use this for initialization
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Holding)
+        {
+            time = Time.time;
+            Holding = false;
+            TimeSetted = true;
+        }
+        if (Time.time - time > 2 && TimeSetted)
+        {
+            SceneManager.LoadScene(1);
+        }
+    }
+    public void SetHolding()
+    {
+        Holding = true;
+    }
+    public void UnsetHolding()
+    {
+        Holding = false;
+        TimeSetted = false;
+        time = 0;
+    }
+}
